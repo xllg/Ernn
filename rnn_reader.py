92ef815d105e86eedaf396b3ca6e4b6717541dfb
xllg <xllg001492>
2018-04-01 17:48:09 +0800

    将char向量缩减到单词最长维度 32 512

--- rnn_reader.py
+++ rnn_reader.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn as nn
-import layers
+from Ernn import layers
 from torch.autograd import Variable
 # ------------------------------------------------------------
 # Network
