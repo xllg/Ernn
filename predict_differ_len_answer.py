82c4718ac8180f0f364096273c16267d97dc3b97
xllg <xllo@outlook.com>
2019-03-17 11:16:41 +0800

    qes type

--- predict_differ_len_answer.py
+++ predict_differ_len_answer.py
@@ -77,20 +77,25 @@ if args.cuda:
 ans_len = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 29]
 qes_len = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 33]
 pas_len = [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 540, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 215, 217, 218, 219, 221, 222, 223, 224, 226, 227, 228, 229, 230, 231, 233, 236, 237, 240, 241, 243, 244, 245, 246, 247, 248, 249, 252, 253, 256, 257, 262, 263, 264, 266, 267, 274, 276, 277, 278, 279, 283, 286, 293, 294, 296, 297, 302, 304, 312, 322, 324, 327, 333, 337, 340, 349, 351, 353, 356, 359, 363, 388, 414, 448, 457, 629, 481, 483, 508, 509]
+qes_type = ["why", "where", "which", "when", "who", "how", "what"]
 
-examples_all = [[] for i in range(len(pas_len))] # differ qes ans
-qids = [[] for i in range(len(pas_len))] # differ qes ans
+examples_all = [[] for i in range(len(qes_type))] # differ qes ans
+qids = [[] for i in range(len(qes_type))] # differ qes ans
 
 with open(args.dataset) as f:
     data = json.load(f)['data']
     for article in data:
         for paragraph in article['paragraphs']:
             context = paragraph['context']
-            text = context.split(' ')
-            id = pas_len.index(len(text))
+            # text = context.split(' ')
+            # id = pas_len.index(len(text))
             for qa in paragraph['qas']:
-                qids[id].append(qa['id'])
-                examples_all[id].append((context, qa['question']))
+                text = qa["question"].split(' ')
+                qes_start = text[0].lower()
+                if qes_start in qes_type:
+                    id = qes_type.index(qes_start)
+                    qids[id].append(qa['id'])
+                    examples_all[id].append((context, qa['question']))
 
 for num, examples in enumerate(examples_all):
     qid = qids[num]
@@ -111,10 +116,13 @@ for num, examples in enumerate(examples_all):
     # model = os.path.splitext(os.path.basename(args.model or 'default'))[0]
     # basename = os.path.splitext(os.path.basename(args.dataset))[0]
     # outfile = os.path.join(args.out_dir, basename + '-' + model + '.preds')
-    outfile = os.path.join(args.out_dir, 'pas-len'+str(pas_len[num])+'.preds')
+    outfile = os.path.join(args.out_dir, 'qes_type_'+str(qes_type[num])+'.preds')
 
     logger.info('Writing results to %s' % outfile)
     with open(outfile, 'w') as f:
         json.dump(results, f)
 
 logger.info('Total time: %.2f' % (time.time() - t0))
+
+
+
